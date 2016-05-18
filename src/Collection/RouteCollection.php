215c9469f9a60f35c01f16fd414f065f0a1fd013
alecgunnar <alecgunnar@gmail.com>
2016-05-18 13:24:33 -0400

    No more prefixed route collection now its a factory

--- src/Collection/RouteCollection.php
+++ src/Collection/RouteCollection.php
@@ -13,5 +13,58 @@ use InvalidArgumentException;
 
 class RouteCollection implements RouteCollectionInterface
 {
-    use RouteCollectionTrait;
+    /**
+     * The list of routes
+     *
+     * @var array
+     */
+    protected $routes = [];
+
+    /**
+     * @inheritDoc
+     */
+    public function withRoute(Route $route, string $name=null): RouteCollectionInterface
+    {
+        if (is_null($name)) {
+            $this->routes[] = $route;
+        } else {
+            $this->routes[$name] = $route;
+        }
+
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function mergeCollection(RouteCollectionInterface $collection): RouteCollectionInterface
+    {
+        $this->routes = array_merge($this->routes, $collection->getRoutes(true));
+
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getRouteByName(string $name): Route
+    {
+        if (!isset($this->routes[$name])) {
+            throw new InvalidArgumentException('A route named ' . $name . ' does not exist.');
+        }
+
+        return $this->routes[$name];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getRoutes(bool $assoc=false): array
+    {
+        if ($assoc) {
+            return $this->routes;
+        }
+
+        return array_values($this->routes);
+    }
 }
